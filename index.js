c2edb3e6677b1550b77c4e068ab81ec134f22300
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-09-02 22:42:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,46 @@
-var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
-    flash = require("connect-flash"),
-    passport = require("passport"),
-    LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
-    Campground = require("./models/campground"),
-    Comment = require("./models/comment"),
-    User = require("./models/user"),
-    seedDB   = require("./seeds")
-    
-var commentRoutes = require("./routes/comments"),
-    campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes = require("./routes/index")
 
-mongoose.connect("mongodb://127.0.0.1:27017/yelp_camp",
- { useUnifiedTopology: true, useNewUrlParser: true })
-app.use(bodyParser.urlencoded({extended:true})); 
-app.set("view engine" ,"ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
-app.use(flash());
-// seedDB();
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
 
-app.use(require("express-session")({
-    secret:"Dugu is the cutuest dog in the world",
-    resave:false,
-    saveUninitialized:false
-}));
+var middlewareObj ={};
+middlewareObj.checkCommentOwnership=async function (req,res,next){
+    if(req.isAuthenticated()){
+        var foundComment = await Comment.findById(req.params.comment_id)
+         if(foundComment.author.id.equals(req.user._id)){   
+                next();
+            }else{
+                req.flash("error","You don't have permission to do That!!!")
+                res.redirect("back");
+            }
+        }else{
+            req.flash("error","You need to be logged in to do That!!!");
+            res.redirect("back");
+    };
 
-app.use(passport.initialize());
-app.use(passport.session());
+}
 
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+middlewareObj.checkCampgroundOwnership =async function (req,res,next){
+    if(req.isAuthenticated()){
+        var foundCampground = await Campground.findById(req.params.id)
+            if(foundCampground.author.id.equals(req.user._id)){
+                next();
+            }else{
+                req.flash("error","You don't have permission to do That!!!")
+                res.redirect("back");
+            }
+        }else{
+            req.flash("error","You need to be logged in to do That!!!");
+            res.redirect("back");
+    };
 
-app.use(function(req,res,next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success= req.flash("success");
-    next();
-});
+}
 
-app.use("/",indexRoutes);
-app.use("/campgrounds/:id/comments",commentRoutes);
-app.use("/campgrounds",campgroundRoutes);
+middlewareObj.isLoggedIn= function (req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error","You need to be logged in to do That!!!");
+    res.redirect("/login");
+    }
 
-app.listen(3000,function(){
-    console.log("The server started");
-});
+module.exports = middlewareObj
